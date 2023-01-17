820e64472d269ac0228e96469411ee447a8471a6
Sandeep <sandepten@gmail.com>
2023-01-17 14:04:30 +0530

    fix: env

--- index.js
+++ index.js
@@ -11,13 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://sandepten:sandeep%402001@sandeepdb.d1e7xoj.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB Connetion Successfull");
   })
